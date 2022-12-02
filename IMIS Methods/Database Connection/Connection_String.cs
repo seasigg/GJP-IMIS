8e8676ed23e301d70a9de5c04bbc54eae7d2f972
MiggyPareja <carlospareja68@gmail.com>
2022-12-02 19:54:58 +0800

    added-logo-to-report/ edited connection string

--- IMIS Methods/Database Connection/Connection_String.cs
+++ IMIS Methods/Database Connection/Connection_String.cs
@@ -12,10 +12,10 @@ namespace GJP_IMIS.IMIS_Methods.Database_Connection
 
         public static SqlConnection con;
         //MIGGY
-        //public static String conn = @"Data Source=MIGGYPAREJA\SQLEXPRESS;Initial Catalog=IMIS;Integrated Security=True";
+        public static String conn = @"Data Source=MIGGYPAREJA\SQLEXPRESS;Initial Catalog=IMIS;Integrated Security=True";
         
         // MAB
-        public static String conn = @"Data Source=DESKTOP-NAARK29\SQLEXPRESS;Initial Catalog=IMIS;Integrated Security=True";
+        //public static String conn = @"Data Source=DESKTOP-NAARK29\SQLEXPRESS;Initial Catalog=IMIS;Integrated Security=True";
 
         //Earl
         //public static String conn = @"Data Source=DESKTOP-S9H3AS7\SQLEXPRESS;Initial Catalog=IMIS;Integrated Security=True";
